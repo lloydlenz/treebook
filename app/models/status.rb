d635c6dc1b77c1853e6f349a1040dd617b66a679
Lloyd Lenz <llenz571@gmail.com>
2014-04-07 21:47:51 -0500

    Add status validations and tests

--- app/models/status.rb
+++ app/models/status.rb
@@ -2,7 +2,10 @@ class Status < ActiveRecord::Base
  	belongs_to 		:user
 
  # Never trust parameters from the scary internet, only allow the white list through.
-	
+	validates :content, presence: true,
+						length: {minimum: 2}
+						
+	validates :user_id, presence: true
 	def create
 		Status.create(params[:user_id, :content])
 	end
